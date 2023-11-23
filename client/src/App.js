8baa583eaee78e1b703da9bb85583fb8e3ff58a5
ztao2 <ztao2@uw.edu>
2023-11-22 17:07:36 -0800

    add google map API

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,140 @@
-import React, {useState, useEffect} from 'react';
+import { Box, Button, ButtonGroup, Flex, HStack, IconButton, Input, SkeletonText, Text} from '@chakra-ui/react'
+import { FaLocationArrow, FaTimes } from 'react-icons/fa'
+
+import {
+  useJsApiLoader,
+  GoogleMap,
+  Marker,
+  Autocomplete,
+  DirectionsRenderer,
+} from '@react-google-maps/api'
+import { useRef, useState } from 'react'
+
+// the center Marker at the beginning
+const center = { lat: 47.625168, lng: -122.337751 }
 
 function App() {
-  const [data, setData] = useState([{}]);
-  useEffect(() => {
-    fetch("/members")
-    .then(res => res.json())
-    .then(data => {
-      setData(data);
-      console.log(data);
-    })
-  }, [])
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null))
+  const [directionsResponse, setDirectionsResponse] = useState(null)
+  const [distance, setDistance] = useState('')
+  const [duration, setDuration] = useState('')
 
-const handleSubmit = (e) => {
-  e.preventDefault();
-}
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const originRef = useRef()
+  /** @type React.MutableRefObject<HTMLInputElement> */
+  const destiantionRef = useRef()
+
+  const lib = ['places'];
+
+  const { isLoaded } = useJsApiLoader({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries: lib,
+  })
+
+  // If it's not loaded
+  if (!isLoaded) {
+    return <SkeletonText />
+  }
+
+
+  async function showRoute() {
+    if (originRef.current.value === '' || destiantionRef.current.value === '') {
+      return
+    }
+    // eslint-disable-next-line no-undef
+    const directionsService = new google.maps.DirectionsService()
+    const results = await directionsService.route({
+      origin: originRef.current.value,
+      destination: destiantionRef.current.value,
+      // eslint-disable-next-line no-undef
+      travelMode: google.maps.TravelMode.DRIVING,
+    })
+    setDirectionsResponse(results)
+    setDistance(results.routes[0].legs[0].distance.text)
+    setDuration(results.routes[0].legs[0].duration.text)
+  }
+
+  function clearRoute() {
+    setDirectionsResponse(null)
+    setDistance('')
+    setDuration('')
+    originRef.current.value = ''
+    destiantionRef.current.value = ''
+  }
 
   return (
-    <div>
-      {data.members?.map((member, i) => {
-        return <p key={i}>{member}</p>;
-      })}
+    <Flex
+      position='relative'
+      flexDirection='column'
+      alignItems='center'
+      h='100vh'
+      w='100vw'
+    >
+      <Box position='absolute' left={0} top={0} h='100%' w='100%'>
+        {/* Google Map Box */}
+        <GoogleMap
+          center={center}
+          zoom={15}
+          mapContainerStyle={{ width: '100%', height: '100%' }}
+          options={{
+            // do not show any more information
+            zoomControl: false,
+            streetViewControl: false,
+            mapTypeControl: false,
+            fullscreenControl: false,
+          }}
+          onLoad={map => setMap(map)}
+        >
+          <Marker position={center} />
+          {directionsResponse && (
+            <DirectionsRenderer directions={directionsResponse} />
+          )}
+        </GoogleMap>
+      </Box>
+      <Box p={4} borderRadius='lg' m={4} bgColor='white' shadow='base' minW='container.md' zIndex='1'>
+        <HStack spacing={2} justifyContent='space-between'>
+          <Box flexGrow={1}>
+            <Autocomplete>
+              <Input type='text' placeholder='Origin' ref={originRef} />
+            </Autocomplete>
+          </Box>
+          <Box flexGrow={1}>
+            <Autocomplete>
+              <Input
+                type='text'
+                placeholder='Destination'
+                ref={destiantionRef}
+              />
+            </Autocomplete>
+          </Box>
 
-      <form onSubmit={handleSubmit} method='post'>
-        <input type="text" id='startPlace' />
-        <br></br>
-        <input type="text" id='endPlace' />
-        <br></br>
-        <input type='submit' value='submit' />
-      </form>
-    </div>
-
-  );
+          <ButtonGroup>
+            <Button colorScheme='pink' type='submit' onClick={showRoute}>
+              Show Route
+            </Button>
+            <IconButton
+              aria-label='center back'
+              icon={<FaTimes />}
+              onClick={clearRoute}
+            />
+          </ButtonGroup>
+        </HStack>
+        <HStack spacing={4} mt={4} justifyContent='space-between'>
+          <Text>Distance: {distance} </Text>
+          <Text>Duration: {duration} </Text>
+          <IconButton
+            aria-label='center back'
+            icon={<FaLocationArrow />}
+            isRound
+            onClick={() => {
+              map.panTo(center)
+              map.setZoom(15)
+            }}
+          />
+        </HStack>
+      </Box>
+    </Flex>
+  )
 }
 
-export default App;
+export default App
