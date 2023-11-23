cd70a29528017ed3aa9fc4b0edc64ea455211bb0
ztao2 <ztao2@uw.edu>
2023-11-23 11:03:41 -0800

    set up idea

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import App from './App'
 import { ChakraProvider, theme } from '@chakra-ui/react'
+import { createRoot } from 'react-dom/client'
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
-  </React.StrictMode>
 );
 
