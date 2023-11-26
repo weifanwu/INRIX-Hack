b0559418df995e4f7e9ee414b2929e559187a117
ztao2 <ztao2@uw.edu>
2023-11-26 00:56:02 -0800

    add pin and infowindow

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps";
 
 
@@ -8,17 +8,20 @@ function App() {
   const center = { lat: 47.625168, lng: -122.337751 };
   const API_KEY = process.env.REACT_APP_MAPS_API_KEY;
   const MAP_ID = process.env.REACT_APP_MAP_ID;
-  // console.log(process.env);
+  const [open, setOpen] = useState(false);
 
   return (
     <APIProvider apiKey={API_KEY}>
       <div style={{height: "100vh", width: "100%"}}>
-      <Map
-        zoom={9}
-        center={center}
-        mapId={MAP_ID}
-      >
-        <AdvancedMarker position={center}></AdvancedMarker>
+      <Map zoom={9} center={center} mapId={MAP_ID}>
+        <AdvancedMarker position={center} onClick={() => setOpen(true)}>
+          <Pin background={"pink"} borderColor={"pink"} glyphColor={"purple"} />
+        </AdvancedMarker>
+        {open && (
+          <InfoWindow position={center} onCloseClick={() => setOpen(false)}>
+            <p>It's a start position!</p>
+          </InfoWindow>
+        )}
       </Map>
       </div>
   </APIProvider>
