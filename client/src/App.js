0265cbb4525f16ffdc1265480804d8d7eb0f8dee
weifan_certik <weifan.wu@certik.com>
2023-11-29 22:38:44 -0800

    chat app

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Map from './components/Map';
 import AddPost from "./components/AddPost";
+import ChatPanel from "./components/Chat";
+
 function App() {
 
   const [directionsResponse, setDirectionsResponse] = useState(null)
@@ -13,6 +15,7 @@ function App() {
         <Routes>
           <Route path='/' exact element={<AddPost setDirections={setDirectionsResponse} />} />
           <Route path='/map' exact element={<Map directions={directionsResponse}/>} />
+          <Route path='/chat' exact element={<ChatPanel/>} />
         </Routes>
       </Router>
     </>
