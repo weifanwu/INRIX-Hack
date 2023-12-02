6025d33fbf7c2529ede1b2d43fa32d710d317200
ztao2 <ztao2@uw.edu>
2023-12-02 14:22:12 -0800

    fix map bug, add match function

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer } from '@react-google-maps/api'
 import { useLocation } from 'react-router-dom';
 import { InfoWindow } from "@react-google-maps/api";
@@ -15,7 +15,6 @@ import { useNavigate } from "react-router-dom";
 
 export default function Map(props) {
     const center = { lat: 47.625168, lng: -122.337751 };
-    let location = useLocation();
     const navigate = useNavigate();
 
     const [map, setMap] = useState(/** @type google.maps.Map */ (null))
@@ -23,35 +22,37 @@ export default function Map(props) {
     const [start, setStart] = useState();
     const [end, setEnd] = useState();
 
+    const [posts, setPosts] = useState([]);
+
+    // let posts = [
+    //   {
+    //     id: 1,
+    //     start: { lat:47.627349, lng: -122.350069 },
+    //     end: {lat: 47.618956, lng: -122.344144}
+    //   },
+    //   {
+    //     id: 2,
+    //     start: { lat: 47.618956, lng: -122.344144 },
+    //     end: { lat: 47.36171, lng: -122.18576 },
+    //   },
+    //   {
+    //     id: 3,
+    //     start: { lat: 47.613086, lng: -122.347959 },
+    //     end: { lat:47.627349, lng: -122.350069 }
+    //   }
+    // ];
 
     // Get all nearest posts from backend later
     // set up new posts data for testing multiple markers
-    let posts = [
-      {
-        id: 1,
-        start: { lat:47.627349, lng: -122.350069 },
-        end: {lat: 47.618956, lng: -122.344144}
-      },
-      {
-        id: 2,
-        start: { lat: 47.618956, lng: -122.344144 },
-        end: { lat: 47.36171, lng: -122.18576 },
-      },
-      {
-        id: 3,
-        start: { lat: 47.36171, lng: -122.18576 },
-        end: { lat: 47.613086, lng: -122.347959 }
-      },
-      {
-        id: 4,
-        start: { lat: 47.613086, lng: -122.347959 },
-        end: { lat:47.627349, lng: -122.350069 }
-      }
-    ];
 
-    function testGetData() {
+    useEffect(() => {
+      console.log("test Get Post Data!");
+      let allPosts = [];
+
+      // declare the async data fetching function
+
       fetch("/testGetPost", {
-        method: "GET"
+          method: "GET"
       }).then((res => res.json()))
       .then(data => {
         console.log(data);
@@ -61,18 +62,49 @@ export default function Map(props) {
           console.log(data[i].start);
           console.log(data[i].end);
           console.log(data[i].post_id);
-          posts.push({
+          allPosts.push({
             id: parseInt(data[i].post_id),
             start: {lat: data[i].start[0], lng: data[i].start[1]},
             end: {lat: data[i].end[0], lng: data[i].end[1]}
           })
         }
-        console.log(posts);
-      })
+          console.log(allPosts);
+          setPosts(allPosts);
+        })
       .catch((err) => console.log(err))
-    }
 
-    testGetData();
+        // Change posts state
+      // setPosts(allPosts);
+      console.log("test posts: ")
+      console.log(posts);
+
+   }, [])
+
+    // async function testGetData() {
+    //   console.log("test Get Post Data")
+    //   fetch("/testGetPost", {
+    //     method: "GET"
+    //   }).then((res => res.json()))
+    //   .then(data => {
+    //     console.log(data);
+    //     console.log("start for loop");
+    //     for (let i = 0; i < data.length; i++) {
+    //       console.log(data[i]);
+    //       console.log(data[i].start);
+    //       console.log(data[i].end);
+    //       console.log(data[i].post_id);
+    //       posts.push({
+    //         id: parseInt(data[i].post_id),
+    //         start: {lat: data[i].start[0], lng: data[i].start[1]},
+    //         end: {lat: data[i].end[0], lng: data[i].end[1]}
+    //       })
+    //     }
+    //     console.log(posts);
+    //   })
+    //   .catch((err) => console.log(err))
+    // }
+
+    // testGetData();
 
     return (
       <div className='show-map'>
@@ -89,7 +121,7 @@ export default function Map(props) {
       }}
       onLoad={(map) => setMap(map)}
     >
-      {/* <Marker position={center} /> */}
+
       {posts.map((object, i) => (
         <Marker
           key={i}
